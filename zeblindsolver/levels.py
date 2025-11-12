5183681c73cc80a7815a4225a51dd0d8800534a4
Tiny Stork <zerealtinystork@gmail.com>
2025-11-12 13:33:08 +0100

    allow level modification for detection debug

--- zeblindsolver/levels.py
+++ zeblindsolver/levels.py
@@ -26,7 +26,7 @@ class QuadLevelSpec:
 LEVEL_SPECS: Sequence[QuadLevelSpec] = (
     QuadLevelSpec(name="L", min_area=0.5, max_area=16.0, min_diameter=0.2, max_diameter=6.0, bucket_cap=8192),
     QuadLevelSpec(name="M", min_area=0.15, max_area=4.0, min_diameter=0.1, max_diameter=3.5, bucket_cap=4096),
-    QuadLevelSpec(name="S", min_area=0.01, max_area=1.5, min_diameter=0.05, max_diameter=2.0, bucket_cap=2048),
+    QuadLevelSpec(name="S", min_area=0.01, max_area=1.5, min_diameter=0.05, max_diameter=2.0, bucket_cap=6000),
 )
 
 LEVEL_MAP: dict[str, QuadLevelSpec] = {level.name: level for level in LEVEL_SPECS}
